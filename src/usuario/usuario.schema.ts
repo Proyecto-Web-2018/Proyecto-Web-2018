e61ad175c92953a165cff0861f93b807925280b3
Marcelo Nieto <marcechelo25@hotmail.com>
2018-05-29 22:03:42 -0500

    contraseña2

--- src/usuario/usuario.schema.ts
+++ src/usuario/usuario.schema.ts
@@ -21,4 +21,15 @@ export const USUARIO_SCHEMA = Joi
       .required(),
     correo: Joi
       .string().email(),
+    /*contraseña2: Joi
+      .number()
+      .srtting()
+      .alphanum()
+      .min (10)
+      .max (30)
+      .lowerCase (1)
+      .upperCase (1)
+      .numeric (1)
+      .symbol (1)
+      .requirementCount (2),*/
   });
