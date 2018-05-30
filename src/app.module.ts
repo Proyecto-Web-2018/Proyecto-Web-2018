5dc3cbf21378a6e1b5540350b862ee0e87582c19
david <davidmoralesp1995@gmail.com>
2018-05-30 01:21:43 -0500

    base en app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario/usuario.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'web2018',
+      entities: [
+        __dirname +
+        '/../**/*.entity{.ts,.js}'
+      ],
+      synchronize: true,
+      ssl: true
+    }),
+    TypeOrmModule.forFeature([
+
+    ])
+
+  ],
   controllers: [AppController, UsuarioController],
   providers: [AppService, UsuarioService],
 })
