eaf51502169813625819cce554ef28258eb0fe38
Marcelo Nieto <marcechelo25@hotmail.com>
2018-05-29 22:37:35 -0500

    usuario pipe

--- src/parametros.controller.ts
+++ src/parametros.controller.ts
@@ -1,6 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller()
 export class ParametrosController {
+
+  @Post('crearUsuario')
+  crearUsuario(@Body(new)){}
 
 }
---
+++ src/pipes/usuario.pipe.ts
@@ -0,0 +1,21 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import * as Joi from 'joi';
+
+@Injectable()
+export class UsuarioPipe implements PipeTransform{
+
+  constructor(private readonly _schema){}
+
+  transform(jsonAValidar: any, metadata: ArgumentMetadata){
+
+    const{error} = Joi.validate(jsonAValidar, this._schema);
+
+    if (!error) {
+      return jsonAValidar;
+    } else {
+      throw new Error('Json no valido');
+
+    }
+  }
+
+}
