07db02c7c90709800b7f91e8cb2780b9fa8ac797
Marcelo Nieto <marcechelo25@hotmail.com>
2018-07-24 13:48:43 -0500

    conexion mysql

--- Backend/config/datastores.js
+++ Backend/config/datastores.js
@@ -34,20 +34,8 @@ module.exports.datastores = {
 
   default: {
 
-    /***************************************************************************
-    *                                                                          *
-    * Want to use a different database during development?                     *
-    *                                                                          *
-    * 1. Choose an adapter:                                                    *
-    *    https://sailsjs.com/plugins/databases                                 *
-    *                                                                          *
-    * 2. Install it as a dependency of your Sails app.                         *
-    *    (For example:  npm install sails-mysql --save)                        *
-    *                                                                          *
-    * 3. Then pass it in, along with a connection URL.                         *
-    *    (See https://sailsjs.com/config/datastores for help.)                 *
-    *                                                                          *
-    ***************************************************************************/
+    adapter: require('sails-mysql'),
+    url: 'mysql://root:root@127.0.0.1:3306/webbackend',
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
 
