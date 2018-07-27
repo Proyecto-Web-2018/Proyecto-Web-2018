14926b9d542a279340fba3a434c5227b2005af79
Marcelo Nieto <marcechelo25@hotmail.com>
2018-07-27 00:00:52 -0500

    login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RUTAS} from "./Rutas";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+
+      RUTAS, {useHash: true}
+
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import * as Cookies from 'js-cookie';
+import {HttpClient} from "@angular/common/http";
+import {CredencialesService} from "../Servicios/credenciales.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = '';
+  password = '';
+  passwordServer = '';
+  usuario = [];
+  usuarioId = 0;
+
+  constructor(private httpClient: HttpClient,
+              private _credenciales: CredencialesService,
+              private _credencialesService: CredencialesService,
+              private _router: Router, ) {
+  }
+
+  ngOnInit() {
+  }
+
+  onNameKeyUpUsername(event: any) {
+    this.username = event.target.value;
+  }
+
+  onNameKeyUpPassword(event: any) {
+    this.password = event.target.value;
+  }
+
+
+  getProfile() {
+    this.httpClient.get(`http://localhost:1337/Usuario?username=${this.username}`).subscribe((data: any[]) => {
+        this.usuario = data;
+        this.passwordServer = this.usuario[0]['password'];
+        this.usuarioId = this.usuario[0]['id'];
+        console.log(this.passwordServer);
+
+        if (this._credencialesService.login(this.password, this.passwordServer) === true) {
+          const rutaHomeUsuario = ['/usuario', this.usuarioId, 'home'];
+          this._router.navigate(rutaHomeUsuario);
+          // const Cookies2 = Cookies.noConflict();
+          Cookies.set('cookieWeb', {username: this.username, id: this.usuarioId, estado: 'true'});
+          // this._actorService.guardarUsuario(this.usuarioId);
+        } else {
+          console.log('error');
+        }
+      }
+    );
+
+
+  }
+
+}
