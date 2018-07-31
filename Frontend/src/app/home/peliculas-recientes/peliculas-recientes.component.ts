396f0918660dddaed73bbd0795722b0d3fbf5bd9
edwar95 <davidmoralesp1995@gmail.com>
2018-07-31 17:24:15 -0500

    peliculas recientes

--- Frontend/src/app/home/peliculas-recientes/peliculas-recientes.component.ts
+++ Frontend/src/app/home/peliculas-recientes/peliculas-recientes.component.ts
@@ -9,6 +9,19 @@ export class PeliculasRecientesComponent implements OnInit {
 
   constructor() { }
 
+  peliculas=[
+    {
+      uri:"https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/03/29/Recortada/img_jatienza_20170829-164347_imagenes_lv_otras_fuentes_20838254_109613609733414_4388983623303299072_n-kkYE-U442025183055F3H-992x558@LaVanguardia-Web.jpg",
+    },
+    {
+      uri:"https://www.hola.com/imagenes/realeza/casa_suecia/20180723127477/magdalena-suecia-inusual-foto-leonore/0-586-859/leonore-suecia-cp-9t-t.jpg",
+    },
+    {
+      uri:"https://www.lavanguardia.com/r/GODO/LV/p5/WebSite/2018/03/29/Recortada/img_jatienza_20170829-164347_imagenes_lv_otras_fuentes_20838254_109613609733414_4388983623303299072_n-kkYE-U442025183055F3H-992x558@LaVanguardia-Web.jpg",
+    },
+  ];
+
+
   ngOnInit() {
   }
 
