2563d5dd98f20b1081238374d1983f4956d49a69
Marcelo Nieto <marcechelo25@hotmail.com>
2018-08-01 01:42:33 -0500

    ...

--- Frontend/src/app/home/peliculas-recientes/peliculas-recientes.component.ts
+++ Frontend/src/app/home/peliculas-recientes/peliculas-recientes.component.ts
@@ -15,24 +15,6 @@ export class PeliculasRecientesComponent implements OnInit {
 
   constructor(private _router: Router, private httpClient: HttpClient) { }
 
-  peliculas=[
-    {
-      uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Breaking_Bad_logo.svg/1200px-Breaking_Bad_logo.svg.png",
-
-    },
-    {
-      uri:"https://upload.wikimedia.org/wikipedia/en/6/61/Breaking_Bad_title_card.png",
-    },
-    {
-      uri:"https://i0.wp.com/freakelitex.com/wp-content/uploads/2017/11/maxresdefault-8.jpg?fit=1920%2C1080",
-    },
-    {
-      uri:"http://www.proyectorfantasma.com.ar/wp-content/uploads/2014/06/VUELVE-BREAKING-BAD-810x456.jpeg",
-    },
-  ];
-
-
-
   ngOnInit() {
     this.getPeliculas()
 
